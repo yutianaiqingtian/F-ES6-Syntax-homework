4cadf85637580a472bc528eb973e33a2fb72e87d
jinghui.zhang <15671678163@163.com>
2020-07-03 22:19:08 +0800

    add practice1 and practice2 implement

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    let {data, column} = input
+
+    const output = [];
+    for (const datum of data) {
+        // [_name, _age, _gender, _birthday] = datum;
+        let obj = {};
+        for (let i = 0; i < column.length; i++) {
+            obj[column[i].name] = datum[i];
+        }
+        output.push(obj)
+    }
+    return output;
+};
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+    const output = new Array(items.length + sections.length);
+
+    sections.sort((a, b) => a.index - b.index)
+    for (let i = 0, offset = 0; i < items.length;) {
+        let section = sections[offset];
+        // console.log(section)
+        if (section != undefined && section.index == i && offset < sections.length) {
+            output[i + offset] = section.content;
+            offset++;
+        } else {
+            output[i + offset] = items[i];
+            i++;
+        }
+    }
+
+    return output;
+}
+
+export {inject};
